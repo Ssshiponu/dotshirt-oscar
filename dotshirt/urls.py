e78ade5b4d6b171db9380facebb1223a5b76b2ca
ssshiponu <sshiponuddin22@gmail.com>
2025-06-04 20:24:19 +0600

    first commit

---
+++ dotshirt/urls.py
@@ -0,0 +1,16 @@
+"""
+URL configuration for dotshirt project.
+"""
+
+from django.apps import apps
+from django.urls import include, path
+from django.contrib import admin
+
+urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
+
+
+    path('admin/', admin.site.urls),
+
+    path('', include(apps.get_app_config('oscar').urls[0])),
+]
